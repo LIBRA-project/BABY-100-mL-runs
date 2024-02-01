aee96ff33e3435108b851d702d4fb9500c81235b
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-02-01 13:59:55 -0500

    added plot_bars to file

--- baby_run_5.py
+++ baby_run_5.py
@@ -20,41 +20,66 @@ def background_sub(measured, background):
 
 
 background = 0.29 * ureg.Bq
-vial_511 = background_sub(0.254 * ureg.Bq, background)
-vial_512 = background_sub(0.289 * ureg.Bq, background)
-vial_513 = background_sub(3.885 * ureg.Bq, background)
-vial_514 = background_sub(0.344 * ureg.Bq, background)
+raw_measurements = {
+    1: {
+        1: 0.254 * ureg.Bq,
+        2: 0.289 * ureg.Bq,
+        3: 3.885 * ureg.Bq,
+        4: 0.344 * ureg.Bq,
+        "background": background,
+    },
+    2: {
+        1: 0.262 * ureg.Bq,
+        2: 0.283 * ureg.Bq,
+        3: 3.998 * ureg.Bq,
+        4: 0.408 * ureg.Bq,
+        "background": background,
+    },
+    3: {
+        1: 0.266 * ureg.Bq,
+        2: 0.293 * ureg.Bq,
+        3: 4.644 * ureg.Bq,
+        4: 0.396 * ureg.Bq,
+        "background": background,
+    },
+    4: {
+        1: 0.275 * ureg.Bq,
+        2: 0.278 * ureg.Bq,
+        3: 5.098 * ureg.Bq,
+        4: 0.505 * ureg.Bq,
+        "background": background,
+    },
+    5: {
+        1: 0.276 * ureg.Bq,
+        2: 0.286 * ureg.Bq,
+        3: 2.823 * ureg.Bq,
+        4: 0.468 * ureg.Bq,
+        "background": background,
+    },
+    6: {
+        1: 0.265 * ureg.Bq,
+        2: 0.281 * ureg.Bq,
+        3: 1.252 * ureg.Bq,
+        4: 0.333 * ureg.Bq,
+        "background": background,
+    },
+    7: {
+        1: 0.262 * ureg.Bq,
+        2: 0.286 * ureg.Bq,
+        3: 0.874 * ureg.Bq,
+        4: 0.387 * ureg.Bq,
+        "background": background,
+    },
+}
 
-vial_521 = background_sub(0.262 * ureg.Bq, background)
-vial_522 = background_sub(0.283 * ureg.Bq, background)
-vial_523 = background_sub(3.998 * ureg.Bq, background)
-vial_524 = background_sub(0.408 * ureg.Bq, background)
-
-vial_531 = background_sub(0.266 * ureg.Bq, background)
-vial_532 = background_sub(0.293 * ureg.Bq, background)
-vial_533 = background_sub(4.644 * ureg.Bq, background)
-vial_534 = background_sub(0.396 * ureg.Bq, background)
-
-vial_541 = background_sub(0.275 * ureg.Bq, background)
-vial_542 = background_sub(0.278 * ureg.Bq, background)
-vial_543 = background_sub(5.098 * ureg.Bq, background)
-vial_544 = background_sub(0.505 * ureg.Bq, background)
-
-vial_551 = background_sub(0.276 * ureg.Bq, background)
-vial_552 = background_sub(0.286 * ureg.Bq, background)
-vial_553 = background_sub(2.823 * ureg.Bq, background)
-vial_554 = background_sub(0.468 * ureg.Bq, background)
-
-vial_561 = background_sub(0.265 * ureg.Bq, background)
-vial_562 = background_sub(0.281 * ureg.Bq, background)
-vial_563 = background_sub(1.252 * ureg.Bq, background)
-vial_564 = background_sub(0.333 * ureg.Bq, background)
-
-vial_571 = background_sub(0.262 * ureg.Bq, background)
-vial_572 = background_sub(0.286 * ureg.Bq, background)
-vial_573 = background_sub(0.874 * ureg.Bq, background)
-vial_574 = background_sub(0.387 * ureg.Bq, background)
-
+measurements_after_background_sub = {
+    i: {
+        j: background_sub(act, raw_measurements[i]["background"])
+        for j, act in raw_measurements[i].items()
+        if j != "background"
+    }
+    for i in raw_measurements
+}
 
 baby_diameter = 1.77 * ureg.inches - 2 * 0.06 * ureg.inches  # from CAD drawings
 baby_radius = 0.5 * baby_diameter
--- simple_tritium_transport_model.py
+++ simple_tritium_transport_model.py
@@ -60,9 +60,12 @@ class Model:
                 if irradiation_start < t < irradiation_stop:
                     return self.TBR * self.neutron_rate
             return 0 * self.TBR * self.neutron_rate
-        
+
         else:
-            if t % (self.exposure_time + self.resting_time) < self.exposure_time and t < self.number_days:
+            if (
+                t % (self.exposure_time + self.resting_time) < self.exposure_time
+                and t < self.number_days
+            ):
                 return self.TBR * self.neutron_rate
             else:
                 return 0 * self.TBR * self.neutron_rate
@@ -128,3 +131,100 @@ def quantity_to_activity(Q):
 
 def activity_to_quantity(A):
     return A / (SPECIFIC_ACT * MOLAR_MASS)
+
+
+import matplotlib.pyplot as plt
+
+
+def plot_bars(measurements, index=None, bar_width=0.35, stacked=True):
+    vial_1_vals = (
+        np.array([sample[1].magnitude for sample in measurements.values()]) * ureg.Bq
+    )
+    vial_2_vals = (
+        np.array([sample[2].magnitude for sample in measurements.values()]) * ureg.Bq
+    )
+    vial_3_vals = (
+        np.array([sample[3].magnitude for sample in measurements.values()]) * ureg.Bq
+    )
+    vial_4_vals = (
+        np.array([sample[4].magnitude for sample in measurements.values()]) * ureg.Bq
+    )
+
+    if index is None:
+        if stacked:
+            index = np.arange(7)
+        else:
+            group_spacing = 1  # Adjust this value to control spacing between groups
+            index = np.arange(7) * (group_spacing / 2 + 1) * bar_width * 4
+
+    if stacked:
+        vial_3_bar = plt.bar(
+            index,
+            vial_3_vals,
+            bar_width,
+            label="Vial 3",
+            color="#FB8500",
+        )
+        vial_4_bar = plt.bar(
+            index,
+            vial_4_vals,
+            bar_width,
+            label="Vial 4",
+            color="#FFB703",
+            bottom=vial_3_vals,
+        )
+        vial_1_bar = plt.bar(
+            index,
+            vial_1_vals,
+            bar_width,
+            label="Vial 1",
+            color="#219EBC",
+            bottom=vial_3_vals + vial_4_vals,
+        )
+        vial_2_bar = plt.bar(
+            index,
+            vial_2_vals,
+            bar_width,
+            label="Vial 2",
+            color="#8ECAE6",
+            bottom=vial_3_vals + vial_4_vals + vial_1_vals,
+        )
+    else:
+        vial_1_bar = plt.bar(
+            index - 1.5 * bar_width,
+            vial_1_vals,
+            bar_width,
+            linewidth=2,
+            edgecolor="white",
+            label="Vial 1",
+            color="#219EBC",
+        )
+        vial_2_bar = plt.bar(
+            index - 0.5 * bar_width,
+            vial_2_vals,
+            bar_width,
+            linewidth=2,
+            edgecolor="white",
+            label="Vial 2",
+            color="#8ECAE6",
+        )
+        vial_3_bar = plt.bar(
+            index + 0.5 * bar_width,
+            vial_3_vals,
+            bar_width,
+            linewidth=2,
+            edgecolor="white",
+            label="Vial 3",
+            color="#FB8500",
+        )
+        vial_4_bar = plt.bar(
+            index + 1.5 * bar_width,
+            vial_4_vals,
+            bar_width,
+            linewidth=2,
+            edgecolor="white",
+            label="Vial 4",
+            color="#FFB703",
+        )
+
+    return index
